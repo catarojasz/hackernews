dab1936b7ad23d3b648c0a81eb0245d14cc2fb9a
cateitor <catalina.rojas@ing.uchile.cl>
2024-07-31 12:13:51 -0400

    feat(mongodb): configure initial MongoDB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { NewsModule } from './news/news.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    NewsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
